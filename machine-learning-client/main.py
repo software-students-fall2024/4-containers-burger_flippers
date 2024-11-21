15ce483d03958e9c6f0f9965e2f41f66d5267deb
anerivs <aneri.shah@nyu.edu>
2024-11-21 03:22:43 -0500

    black formatting fixes

--- machine-learning-client/main.py
+++ machine-learning-client/main.py
@@ -5,11 +5,11 @@ Accepts user audio input, creates a transcription, and sends it back to the weba
 import os
 import datetime
 import whisper
-from flask import Flask,jsonify, request
+from flask import Flask, jsonify, request
 import pymongo
 from dotenv import load_dotenv
 
-#load dotenv
+# load dotenv
 load_dotenv()
 
 mongo_uri = os.getenv("MONGO_URI")
@@ -19,15 +19,15 @@ flask_port = int(os.getenv("FLASK_PORT"))
 client = pymongo.MongoClient(mongo_uri)
 db = client["sensor_data"]
 
-#init flask
+# init flask
 app = Flask(__name__)
 
-#loading whisper model
+# loading whisper model
 print("loading whisper model...")
 model = whisper.load_model("base")
 print("whisper model sucessfully loaded!")
 
-#receives user audio input, creates transcriptions, adds to database
+# receives user audio input, creates transcriptions, adds to database
 # and returns transcription to webapp
 @app.route("/transcribe", methods=["POST"])
 def transcribe():
@@ -36,30 +36,30 @@ def transcribe():
     saving the transcription to the database, and returning the result.
     """
     try:
-        #receive user audio input, saving it to tmp folder
+        # receive user audio input, saving it to tmp folder
         file = request.files["file"]
         file_path = f"/tmp/{file.filename}"
         file.save(file_path)
-        #to test audio:
-        #test_file_name = "test.mp3"
-        #test_file_path = "./tmp/test.mp3"
+        # to test audio:
+        # test_file_name = "test.mp3"
+        # test_file_path = "./tmp/test.mp3"
 
-        #transcribing audio file
-        print('transcribing new input...')
+        # transcribing audio file
+        print("transcribing new input...")
         result = model.transcribe(file_path)
         transcription = result["text"]
-        print('transcribed')
+        print("transcribed")
 
-        #saving transcription to database
+        # saving transcription to database
         data = {
             "filename": file.name,
             "transcription": transcription,
-            "timestamp": datetime.datetime.now()
+            "timestamp": datetime.datetime.now(),
         }
         db["history"].insert_one(data)
         print("saved transcription to history")
 
-        #return transcription
+        # return transcription
         return jsonify({"transcription": transcription}), 200
 
     except FileNotFoundError as e:
@@ -71,10 +71,10 @@ def transcribe():
 
 
 if __name__ == "__main__":
-    print('connecting to mongodb database')
+    print("connecting to mongodb database")
     try:
         app.run(host=flask_host, port=flask_port)
-    except KeyboardInterrupt: #stop if received keyboard input
+    except KeyboardInterrupt: # stop if received keyboard input
         print("detected interrupt-shutting down")
     finally:
         client.close()
