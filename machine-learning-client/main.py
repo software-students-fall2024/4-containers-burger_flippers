3a1e41a597e0bdc68ae84a3041d8836adf1beb76
anerivs <aneri.shah@nyu.edu>
2024-11-21 02:37:54 -0500

    fixed linting errors

--- machine-learning-client/main.py
+++ machine-learning-client/main.py
@@ -29,24 +29,31 @@ print("loading whisper model...")
 model = whisper.load_model("base")
 print("whisper model sucessfully loaded!")
 
-#receives user audio input, creates transcriptions, adds to database, and returns transcription to webapp
+#receives user audio input, creates transcriptions, adds to database
+# and returns transcription to webapp
 @app.route("/transcribe", methods=["POST"])
 def transcribe():
+    """
+    Handle transcription requests by processing an audio file,
+    saving the transcription to the database, and returning the result.
+    """
     try:
-        #receive user audio input
-        file = request.files["file"]
-        file_path = f"/tmp/{file.filename}"
-        file.save(file_path)
+        #receive user audio input, saving it to tmp folder
+        #file = request.files["file"]
+        #file_path = f"/tmp/{file.filename}"
+        #file.save(file_path)
+        test_file_name = "test.mp3"
+        test_file_path = "./tmp/test.mp3"
 
         #transcribing audio file
         print('transcribing new input...')
-        result = model.transcribe(file_path)
+        result = model.transcribe(test_file_path)
         transcription = result["text"]
         print('transcribed')
 
         #saving transcription to database
         data = {
-            "filename": file.filename,
+            "filename": test_file_name,
             "transcription": transcription,
             "timestamp": datetime.datetime.now()
         }
@@ -56,9 +63,12 @@ def transcribe():
         #return transcription
         return jsonify({"transcription": transcription}), 200
 
-    except Exception as e:
-        print("error transcribing")
-        return jsonify({"error": str(e)}), 500
+    except FileNotFoundError as e:
+        print(f"File error: {e}")
+        return jsonify({"error": "File not found"}), 400
+    except pymongo.errors.PyMongoError as e:
+        print(f"Database error: {e}")
+        return jsonify({"error": "Database error"}), 500
 
 
 if __name__ == "__main__":
